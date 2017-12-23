98190a0c5742b8088fcc87a28f2e5a84364254fe
Karen Vicente <karenyasmin37@gmail.com>
2017-12-23 21:08:33 -0200

    Corrigido bugs e adicionado página index de forma dinamica

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,8 +20,127 @@ jQuery(function ($) {
 
 }); // JQuery end
 
-function build_tools_icons() {
+function skills() {
+	var skills = [
+		{linguagem: 'PHP', porcentagem: 90},
+		{linguagem: 'Java', porcentagem: 65},
+		{linguagem: 'Python', porcentagem: 75},
+		{linguagem: 'HTML & CSS', porcentagem: 80},
+		{linguagem: 'JavaScript & JQuery', porcentagem: 65},
+		{linguagem: 'Bootstrap', porcentagem: 80}
+	];
+	
+	var html = '';
+	for (i = 0; i < skills.length; i++) {
 
+		html += '<div class="progress-item">' + 
+					'<span class="progress-title">' + skills[i].linguagem + '</span>' +
+					'<div class="progress">' + 
+						'<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: ' + skills[i].porcentagem + '%">' + 
+						'</div>' +
+					'</div>' +
+				'</div>';	
+	}
+	$('#skills').html(html);
+}
+
+function language() {
+	var language = [
+		{idioma: 'Inglês', nivel: 'Intermediário', observacao: 'Conversação, fala e escrita'},
+		{idioma: 'Espanhol', nivel: 'Básico', observacao: 'Conversação, fala e escrita'}
+	];
+	
+	var html = '';
+	html += '<div class="row">';
+	for (i = 0; i < language.length; i++) {
+		if ((i % 2)== 0 && (i != 0)) {
+			html += '</div><div class="row">';
+		}
+	
+		html += '<div class="col-md-6">' +
+					'<div class="content-item">' + 
+						'<h3>' + language[i].idioma + '</h3>' +
+						'<h4>' + language[i].nivel + '</h4>' +
+						'<p>' + language[i].observacao + '</p>' +
+					'</div>' +
+				'</div>';				
+	}
+	$('#language').html(html);
+}
+
+function experience() {
+	var experience = [
+		{empresa: 'Intero Brasil', cargo: 'Analista de desenvolvimento Jr', cidade: 'São José dos Campos', estado: 'SP', ano_inicio:'2017', ano_fim:''},
+		{empresa: 'Season Consultoria e Treinamentos Avançados em Informática', cargo: 'Auxiliar de desenvolvimento de sistemas', cidade: 'São José dos Campos', estado: 'SP', ano_inicio:'2017', ano_fim:'2017'},
+		{empresa: 'Season Consultoria e Treinamentos Avançados em Informática', cargo: 'Estágio em desenvolvimento Web', cidade: 'São José dos Campos', estado: 'SP', ano_inicio:'2016', ano_fim:'2017'}
+	];
+	
+	var html = '';
+	for (i = 0; i < experience.length; i++) {
+		var fim = 'Atualmente';
+		if (experience[i].ano_fim != '')
+			fim = experience[i].ano_fim;
+	
+		html += '<div class="content-item">' +
+					'<small>' + experience[i].ano_inicio + ' - ' + fim + '</small>' +
+					'<h3>' + experience[i].empresa + '</h3>' +
+					'<h4>' + experience[i].cargo + '</h4>' +
+					'<p>' + experience[i].cidade + ' - ' + experience[i].estado + '</p>' + 
+				'</div>';
+	}
+	$('#experience').html(html);
+}
+
+function education() {
+	var education = [
+		{instituicao: 'Fatec São José dos Campos Prof. Jessen Vidal', curso: 'Tecnólogo - Análise e Desenvolvimento de Sistemas', cidade: 'São José dos Campos', estado: 'SP', ano_inicio:'2013', ano_fim:'2016'},
+		{instituicao: 'Colégio Joseense', curso: 'Técnico em Informática', cidade: 'São José dos Campos', estado: 'SP', ano_inicio:'2009', ano_fim:'2011'},
+		{instituicao: 'Colégio Joseense', curso: 'Ensino Médio', cidade: 'São José dos Campos', estado: 'SP', ano_inicio:'2009', ano_fim:'2011'}
+	];
+	
+	var html = '';
+	for (i = 0; i < education.length; i++) {
+		var fim = 'Atualmente';
+		if (education[i].ano_fim != '')
+			fim = education[i].ano_fim;
+	
+		html += '<div class="content-item">' +
+					'<small>' + education[i].ano_inicio + ' - ' + fim + '</small>' +
+					'<h3>' + education[i].curso + '</h3>' +
+					'<h4>' + education[i].instituicao + '</h4>' +
+					'<p>' + education[i].cidade +  ' - ' + education[i].estado + '</p>' + 
+				'</div>';
+	}
+	$('#education').html(html);
+}
+
+function certifications() {
+	
+	var certifications = [
+		{instituicao: 'Treinaweb', curso: 'Java Básico', certificado: 'certifications/treinaweb/java-basico.pdf'},
+		{instituicao: 'Treinaweb', curso: 'Lógica Orientada a Objetos', certificado: 'certifications/treinaweb/logica-orientada-a-objetos-completo.pdf'},
+		{instituicao: 'Treinaweb', curso: 'PHP Básico', certificado: 'certifications/treinaweb/php-basico.pdf'},
+		{instituicao: 'Treinaweb', curso: 'Angular 2 Completo', certificado: 'certifications/treinaweb/angular-2-compelto.pdf'},
+		{instituicao: 'Treinaweb', curso: 'Desenvolvendo aplicações mobile com Ionic 2 Completo', certificado: 'certifications/treinaweb/desenvolvendo-aplicacoes-mobile-com-ionic2-completo.pdf'},
+	];
+	
+	var html = '';
+	html += '<div class="item active">';
+	for (i = 0; i < certifications.length; i++) {
+		if ((i % 4)== 0 && (i != 0)) {
+			html += '</div><div class="item">';
+		}
+		html += '<div class="col-md-6">' + 
+					'<div class="certifications-item">' +
+						'<h3><a href="'+ certifications[i].certificado +'" target="_blank">' + certifications[i].curso +'</a></h3>' +
+						'<p>' + certifications[i].instituicao + '</p>' +
+					'</div>' +
+				'</div>';
+	}
+	$('#certifications').html(html);
+}
+
+function tools_icons() {
 	var tool_icons =['devicon-angularjs-plain', 
 					  'devicon-bootstrap-plain', 
 					  'devicon-apache-line-wordmark',
@@ -42,15 +161,13 @@ function build_tools_icons() {
   	var html = ''
 	html += '<ul class="list-inline list-icons">'
 	for (i = 0; i < tool_icons.length; i++) {		
-		if ((i % 7)== 0) {
+		if ((i % 7)== 0 && (i != 0)) {
 			html += '</ul><br>' + 
 					'<ul class="list-inline list-icons">';
 		}
 		html += '<li class="list-inline-item">' +
 					'<i class="'+ tool_icons[i] +'"></i>' + 
-				'</li>';
-			
-		
+				'</li>';	
 	}
 	$('#tools_icons').html(html);
 }
@@ -63,7 +180,12 @@ $(document).ready(function() {
 		interval: 4000,
 	});
 	
-	build_tools_icons();
+	tools_icons();
+	certifications();
+	education();
+	experience();
+	language();
+	skills();
 	
 });
 
